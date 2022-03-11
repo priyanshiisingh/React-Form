b9df8acf539f0cd5b2ba446aeb217e1b7dd40c31
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-03-11 12:22:24 +0530

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import BrandIndex from "./Layouts/Brand/BrandIndex";
 import FooterIndex from "./Layouts/Footer/FooterIndex";
+import Header from "./Layouts/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <BrandIndex />
+      <Header />
       <FooterIndex />
     </div>
   );
---
+++ src/Layouts/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="hmain">
+      <h1>Full-Stack Engineer</h1>
+      <h6>REMOTE OR SF BAY AREA /PRODUCT – ENGINEERING /FULL-TIME</h6>
+    </div>
+  );
+};
+
+export default Header;
