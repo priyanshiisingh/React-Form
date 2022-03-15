6ec0f4378e7337de9da35f3e4775c1f7f4baa2e5
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-03-15 20:35:12 +0530

    styles

--- src/assets/styles/FormStyles.tsx
+++ src/assets/styles/FormStyles.tsx
@@ -1,17 +1,14 @@
 import styled from "styled-components";
 
-const Form = styled.form`
-  width: fit-content;
-
-  border: 1px solid black;
-`;
-
 const DivBody = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid black;
+`;
+
+const Form = styled.form`
+  width: fit-content;
 `;
 
 export { Form, DivBody };
