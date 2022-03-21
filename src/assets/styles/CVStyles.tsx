8636e0b83f9c1ea3660eae382350f7058600ecbf
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-03-21 09:59:41 +0530

    styles

--- src/assets/styles/CVStyles.tsx
+++ src/assets/styles/CVStyles.tsx
@@ -10,13 +10,8 @@ const CVDiv = styled.div`
 `;
 
 const CVInput = styled.input`
-  border: 1px solid #e2e2e2;
-  border-radius: 3px;
   height: 0;
-  width: 0;
-  font-size: 16px;
-  line-height: 1.4;
-  color: #515357;
+  width: 0; ;
 `;
 
 const CVLabel = styled.label`
@@ -24,11 +19,15 @@ const CVLabel = styled.label`
   letter-spacing: 2px;
   border-radius: 3px;
   height: 40px;
-  background-color: #e2e2e2;
+  background-color: #ebecf0;
   font-size: 14px;
   line-height: 1.4;
   color: #737578;
   padding: 10px 15px;
+
+  &:hover {
+    background-color: #e2e2e2;
+  }
 `;
 
 const CVsvg = styled.svg`
--- src/assets/styles/SelectOpStyles.tsx
+++ src/assets/styles/SelectOpStyles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Option = styled.option`
   border: 1px solid #e2e2e2;
-  background-color: #e2e2e2;
+  background-color: #c0c0c0;
   border-radius: 3px;
   height: 40px;
   width: 500px;
@@ -29,6 +29,10 @@ const Select = styled.select`
   line-height: 1.4;
   color: #515357;
   padding: 4px 4px 4px 15px;
+
+  &:hover {
+    background-color: #c0c0c0;
+  }
 `;
 
 export { Option, Select, SVG };
