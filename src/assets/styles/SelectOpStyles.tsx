0bfc52a19865f7aba00214fa7a8239fde2044c3a
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-03-23 11:45:41 +0530

    select arrows changes

--- src/assets/styles/SelectOpStyles.tsx
+++ src/assets/styles/SelectOpStyles.tsx
@@ -29,6 +29,13 @@ const Select = styled.select`
   line-height: 1.4;
   color: #515357;
   padding: 4px 4px 4px 15px;
+  background: #e2e2e2
+    url("data:image/svg+xml;utf8,<svg viewBox='0 0 140 140' width='24' height='26' xmlns='http://www.w3.org/2000/svg'><g><path d='m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z' fill='grey'/></g></svg>")
+    no-repeat;
+  background-position: right 5px top 50%;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
 
   &:hover {
     background-color: #c0c0c0;
